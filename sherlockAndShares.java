a19e6db8ec746c53474b5b170b8ad001cb477879
Benedict Ortiz <benortiz1391@gmail.com>
2022-06-22 08:32:14 -0600

    edits

--- sherlockAndShares.java
+++ sherlockAndShares.java
@@ -16,6 +16,8 @@ public class sherlockAndShares {
     Your solution must solve each problem in 1 second or less.
      */
     public static void main(String[] args) {
+        System.out.println(squares(3, 9));
+        System.out.println(squares(17, 24));
         System.out.println(squares(1, 1000000000));
     }
 
