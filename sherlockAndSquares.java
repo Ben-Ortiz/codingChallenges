68659d3c2d3cefbc24656926b453b5b26f0d43b2
Benedict Ortiz <benortiz1391@gmail.com>
2022-06-22 08:42:55 -0600

    edits

--- sherlockAndSquares.java
+++ sherlockAndSquares.java
@@ -1,4 +1,4 @@
-public class sherlockAndShares {
+public class sherlockAndSquares {
     /*
     Watson likes to challenge Sherlock's math ability. He will provide a starting and ending value describing a range of integers. Sherlock must determine the number of square integers within that range, inclusive of the endpoints (note that a square integer is an integer which is the square of an integer,
     e.g. 1, 4, 9, 16, 25, 36, 49).
